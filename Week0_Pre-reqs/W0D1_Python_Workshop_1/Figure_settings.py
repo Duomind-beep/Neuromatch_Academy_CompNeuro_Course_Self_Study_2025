7cf18c2030b252038ebaf46519b6eda4409abf9f
Glynn St. Juste <gstjuste95@gmail.com>
2025-09-26 01:00:13 -0400

    Delete Week0_Pre-reqs/W0D1_Python_Workshop_1/Figure_settings.py

--- Week0_Pre-reqs/W0D1_Python_Workshop_1/Figure_settings.py
+++
@@ -1,5 +0,0 @@
-# Figure settings
-import logging
-logging.getLogger('matplotlib.font_manager').disabled = True
-import ipywidgets as widgets  # interactive display
-%config InlineBackend.figure_format = 'retina'
